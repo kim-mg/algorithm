ec4cfa82b514902d54ab581cd77d618f54983b7d
kim-mg <audrjs3535@naver.com>
2022-04-17 23:52:19 +0900

    17/04/2022

--- baekjoon/test5.py
+++ baekjoon/test5.py
@@ -1,41 +1,37 @@
 import sys
 input = sys.stdin.readline
 
-# failed
+def gcd(x, y):
+	if x % y == 0:
+		return y
+	return gcd(y, x%y)
 
-def solution(n, k):
-	r = 1
-	num = 1
-	for i in range(1, n + 1):
-		r *= i
-		if i <= k:
-			num *= i
-		if i <= (n-k):
-			num *= i
-	print(int(r / num) % 10007)
+def get_divisors(x):
+	l = []
+	t = []
+	for i in range(1, (x**2) + 1):
+		if x % i == 0:
+			l.append(i)
+			t.append(x//i)
+	print(l,t)
 
-# ====================================================
-# best time part_1
-def solution2(n, k):
-	sto = [[1 for _ in range(k+1)] for _ in range(n+1)]
-
-	for i in range(1, k+1):
-		for j in range(i+1, n+1):
-			sto[j][i] = (sto[j-1][i-1] + sto[j-1][i]) % 10007
-	
-	print(sto[n][k])
-
-# ====================================================
-# best time part_2
-def solution3(n, k):
-	r = 1
-	for i in range(k):
-		r *= n-i
-		r //= i+1
-	print(r%10007)
+def solution(n, l):
+	min_n = l[-1]
+	max_n = 0
+	for i in range(1, n+1):
+		tmp = l[i] - l[i-1]
+		if tmp < min_n:
+			min_n = tmp
+		if tmp > max_n:
+			max_n = tmp
+	cd = gcd(max_n, min_n)
+	get_divisors(cd)
+	pass
 
 if __name__ == "__main__":
-	n, k = map(int, input().split())
-	solution(n, k)
-	solution2(n, k)
-	solution3(n, k)
+	n = int(input())
+	l = []
+	for _ in range(n):
+		l.append(int(input()))
+	l.sort()
+	solution(n, l)
