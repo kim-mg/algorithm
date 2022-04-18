01ae3e5b4de42c15fe12b05ad8e8ea59dc3e1ce3
kim-mg <audrjs3535@naver.com>
2022-04-18 21:00:09 +0900

    18/04/2022

--- baekjoon/number_theory_and_combinatorics/zero_of_factorial_1676.py
+++ baekjoon/number_theory_and_combinatorics/zero_of_factorial_1676.py
@@ -12,6 +12,11 @@ def count_ele(x, std):
 def solution(n):
 	print(min(count_ele(n, 2), count_ele(n, 5)))
 
+# =====================================================
+# best time
+def solution2(n):
+	print(n//5 + n//25 + n//125)
+
 if __name__ == "__main__":
 	n = int(input())
 	solution(n)
