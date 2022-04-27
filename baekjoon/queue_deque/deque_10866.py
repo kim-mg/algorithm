b3709898cd0f16e8b34bce1929043f5474ec8706
kim-mg <audrjs3535@naver.com>
2022-04-27 17:10:04 +0900

    27/04/2022

--- baekjoon/queue_deque/deque_10866.py
+++ baekjoon/queue_deque/deque_10866.py
@@ -4,26 +4,82 @@ input = sys.stdin.readline
 
 def solution():
 	n = int(input())
-	dq = ['0'] * n
-	f = 0
-	r = 0
-	s = 0
-	r = ""
+	dq = [[0,'0'] for _ in range(n)]
+	h = [0,0,0]
+	line = ""
 
 	def pf(num):
-		dq[f] = num
-		f -= 1
-		if f < 0:
-			f = n - 1
+		if dq[h[0]][0] == 1:
+			h[0] -= 1
+			if h[0] < 0:
+				h[0] = n - 1
+		dq[h[0]][1] = num
+		dq[h[0]][0] = 1
+		h[2] += 1
+		h[0] -= 1
+		if h[0] < 0:
+			h[0] = n - 1
 	
 	def pb(num):
-		dq[r] = num
-		r += 1
+		if dq[h[1]][0] == 1:
+			h[1] += 1
+			if h[1] > n - 1:
+				h[1] = 0
+		dq[h[1]][1] = num
+		dq[h[1]][0] = 1
+		h[2] += 1
+		h[1] += 1
+		if h[1] > n - 1:
+			h[1] = 0
 
 	def ppf():
-		if s == 0:
+		if h[2] == 0:
 			return "-1"
-		t = dq[f + 1]
+		if dq[h[0]][0] == 0:
+			h[0] += 1
+			if h[0] > n - 1:
+				h[0] = 0
+		t = dq[h[0]][1]
+		dq[h[0]][0] = 0
+		h[2] -= 1
+		return t
+	
+	def ppb():
+		if h[2] == 0:
+			return "-1"
+		if dq[h[1]][0] == 0:
+			h[1] -= 1
+			if h[1] < 0:
+				h[1] = n - 1
+		t = dq[h[1]][1]
+		dq[h[1]][0] = 0
+		h[2] -= 1
+		return t
+
+	def size():
+		return str(h[2])
+
+	def empty():
+		if h[2]:
+			return "0"
+		return "1"
+	
+	def front():
+		if h[2] == 0:
+			return "-1"
+		t = h[0] + 1
+		if t > n - 1:
+			t = 0
+		return dq[t][1]
+	
+	def back():
+		if h[2] == 0:
+			return "-1"
+		t = h[1] - 1
+		if h[1] < 0:
+			h[1] = n - 1
+		return dq[t][1]
+
 	for _ in range(n):
 		cm = input().split()
 		if len(cm) > 1:
@@ -31,11 +87,19 @@ def solution():
 				pf(cm[1])
 			if cm[0] == "push_back":
 				pb(cm[1])
-			s += 1
 		elif cm[0] == "pop_front":
-			r += ppf() + "\n"
-			s -= 1
-	pass
+			line += ppf() + "\n"
+		elif cm[0] == "pop_back":
+			line += ppb() + "\n"
+		elif cm[0] == "size":
+			line += size() + "\n"
+		elif cm[0] == "empty":
+			line += empty() + "\n"
+		elif cm[0] == "front":
+			line += front() + "\n"
+		elif cm[0] == "back":
+			line += back() + "\n"
+	print(line.rstrip("\n"))
 
 def solution2():
 	n = int(input())
@@ -83,4 +147,4 @@ def solution2():
 	print(r.rstrip('\n'))
 
 if __name__ == "__main__":
-	solution2()
+	solution()
