c82d4b91d2fd89ba046179720101e69836c124d7
kim-mg <audrjs3535@naver.com>
2022-05-02 20:06:02 +0900

    02/05/2022

--- baekjoon/test5.py
+++ baekjoon/test5.py
@@ -1,8 +1,6 @@
 import sys
 input = sys.stdin.readline
 
-p = 1000000007
-
 def facto(n):
 	r = 1
 	for i in range(2, n+1):
@@ -19,7 +17,7 @@ def mod_square(n, k, p):
 	else:
 		return (mod * mod) % p
 
-def solution(n, k):
+def solution(n, k, p):
 	r1 = 1
 	r2 = 1
 	
@@ -28,6 +26,36 @@ def solution(n, k):
 		r2 = (r2 * i) % p
 	print(((r1 % p) * mod_square(r2, p-2, p)) % p)
 
+# ============================================================
+# best time
+def euclidean(x, y):
+	q = []
+	while y:
+		q.append(x // y)
+		(x, y) = (y, x % y)
+	q.pop()
+
+	a, b = 0, 1
+	for i in q[-1: :-1]:
+		a, b = b, a - i*b
+	print(q, a, b)
+	return x, a, b
+
+def solution2(n, k, p):
+	k = min(k, n-k)
+	if k == 0:
+		return 1
+	top = n
+	bot = 1
+	n += 1
+	for i in range(2, k+1):
+		top = top*(n-i)%p
+		bot = bot*i%p
+	g, inv, _ = euclidean(bot, p)
+	return top*inv%p
+
 if __name__ == "__main__":
 	n, k = map(int, input().split())
-	solution(n, k)
+	p = 1000000007
+	solution(n, k, p)
+	print(solution2(n, k, p))
