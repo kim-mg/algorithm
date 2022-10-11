d2d8704d9a58d24d9995a08a8c53ee9edff14ddb
kim-mg <audrjs3535@naver.com>
2022-10-11 19:14:23 +0900

    14 b_s 1solv update

--- baekjoon/14 binary_search/KthNum_1300.py
+++ baekjoon/14 binary_search/KthNum_1300.py
@@ -20,6 +20,20 @@ def solution():
 			left = mid + 1
 	print(left)
 
+def solution2():
+	n = int(input())
+	k = int(input())
+
+	left = 1
+	right = k
+	while left < right:
+		mid = (right + left) // 2
+		m = int(mid**0.5)
+		if 2 * sum(min(n, mid // i) for i in range(1, m + 1)) - m**2 >= k: right = mid
+		else:
+			left = mid + 1
+	print(left)
 
 if __name__ == "__main__":
-	solution()
+	# solution()
+	solution2()
